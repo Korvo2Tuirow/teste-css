e8011111bc59dc0082fd751b4e7f98b79e3071d4
Korvo2Tuirow <korvo2irow@live.com>
2023-01-28 13:04:09 -0300

    teste

--- script.js
+++ script.js
@@ -2,12 +2,8 @@ let i;
 let num;
 let n;
 
-for(i=0; i<=5; i++){
-    n = Math.floor(Math.random()*61);
-  num = i+n;
-
-  let array = [];
-  array.push(num);
-    console.log(array);
-};
-
+for (i = 0; i <= 5; i++) {
+  n = Math.floor(Math.random() * 61);
+  let num = i + n;
+  console.log(num);
+}
